d00409a141142be5357f14c9788d1e6aea99e35f
YmirNi <102678760+YmirNi@users.noreply.github.com>
2022-04-13 12:18:48 +0300

    13.04.22
    RegistrationTest

--- src/test/java/Registration.java
+++ src/test/java/Registration.java
@@ -9,50 +9,24 @@ import org.testng.annotations.Test;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
-public class LoginTest {
+public class Registration {
 
     WebDriver wd;
 
     @BeforeMethod
-
     public void init(){
         wd = new ChromeDriver();
-        //wd = new FirefoxDriver();
         wd.manage().window().maximize();
         wd.navigate().to("https://contacts-app.tobbymarshall815.vercel.app/home");
         wd.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
     }
 
-    //@Test
-    //public void successLoginTest() {
-        //WebElement element1 = wd.findElement(By.tagName("a"));
-
-        //WebElement elId = wd.findElement(By.id("root"));
-        //WebElement elClass = wd.findElement(By.className("container"));
-        //WebElement elName = wd.findElement(By.name("nameEx"));
-
-        //WebElement elLink = wd.findElement(By.linkText("Home"));
-        //WebElement elPLink = wd.findElement(By.partialLinkText("HOM"));
-
-        //WebElement element2 = wd.findElement(By.cssSelector("a[href='/login']"));
-        //WebElement element3 = wd.findElement(By.cssSelector("[href='/login']"));
-        //WebElement element4 = wd.findElement(By.cssSelector("a"));
-
-        //WebElement element5 = wd.findElement(By.cssSelector("#root"));
-
-        //List <WebElement> list = wd.findElements(By.tagName("a"));
-        //WebElement element = list.get(2);
-        //element.click(); //click login button
-        //element.clear();
-        //element.sendKeys("Hello"); //fill LoginRegistration form
-    //}
-
     @Test
-    public void fillLoginFormTest(){
+    public void successRegistrationTest() {
         WebElement element = wd.findElement(By.cssSelector("[href='/login']"));
         element.click();
 
-        List <WebElement> list = wd.findElements(By.tagName("input"));
+        List<WebElement> list = wd.findElements(By.tagName("input"));
 
         WebElement inputEmail = list.get(0);
         WebElement inputPassword = list.get(1);
@@ -65,13 +39,13 @@ public class LoginTest {
         inputPassword.clear();
         inputPassword.sendKeys("Nnoa12345$");
 
-        WebElement buttonLogin = wd.findElement(By.tagName("button"));
-        buttonLogin.click();
+        List <WebElement> list1 = wd.findElements(By.tagName("button"));
+        WebElement buttonRegistration = list1.get(1);
+        buttonRegistration.click();
     }
 
-
     @Test
-    public void negativeLoginTest() {
+    public void negativeRegistrationTest() {
         WebElement element = wd.findElement(By.cssSelector("[href='/login']"));
         element.click();
 
@@ -88,13 +62,15 @@ public class LoginTest {
         inputPassword.clear();
         inputPassword.sendKeys("Nnoa12345$");
 
-        WebElement buttonLogin = wd.findElement(By.tagName("button"));
-        buttonLogin.click();
+        List <WebElement> list1 = wd.findElements(By.tagName("button"));
+        WebElement buttonRegistration = list1.get(1);
+        buttonRegistration.click();
     }
 
     @AfterMethod
 
     public void tearDown(){
-        wd.quit();
+        //wd.quit();
     }
+
 }
